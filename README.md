e5a42a3c1a3400bf9b206fe3033b22670d91b2cc
SweetPicklee <32518710+SweetPicklee@users.noreply.github.com>
2019-02-14 16:30:04 +0000

    first commit added patatap to portfolio

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio
+Francis Clark Portfolio
---
+++ js/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+	// Add smooth scrolling to all links in navbar + footer link
+	$(".navbar a, footer a[href='#home']").on('click', function(event) {
+		event.preventDefault();
+
+		// Store hash
+		var hash = this.hash;
+
+		// Using jQuery's animate() method to add smooth page scroll
+		// The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+		$('html, body').animate(
+			{
+				scrollTop: $(hash).offset().top - 100,
+			},
+			600,
+			function() {
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				window.location.hash = hash;
+			},
+		);
+	});
+	$window = $(window);
+
+	$('[data-type="background"]').each(function() {
+		var $scroll = $(this);
+
+		$(window).scroll(function() {
+			var yPos = -(($window.scrollTop() - $scroll.offset().top) / $scroll.data('speed'));
+
+			// background position
+			var coords = '50% calc(50% + ' + yPos + 'px)';
+
+			// move the background
+			$scroll.css({
+				backgroundPosition: coords,
+			});
+		});
+	});
+});
