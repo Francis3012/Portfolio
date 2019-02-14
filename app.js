7fef508601fc167ff77ae25f8ef36175edf4afea
SweetPicklee <32518710+SweetPicklee@users.noreply.github.com>
2019-02-14 20:36:09 +0000

    fixed Partials and links to bootstrap moved html to ejs file and setup my app

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.redirect('/portfolio');
+});
+
+app.get('/portfolio', (req, res) => {
+	res.render('portfolio');
+});
+
+app.listen(3000);
